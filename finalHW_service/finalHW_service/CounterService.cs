576b1a657905ed6510826f749d2ad101c3a09a76
dasharus01 <you@example.com>
2022-10-19 22:23:54 +0300

    Code for sercice

---
+++ finalHW_service/finalHW_service/CounterService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using static finalHW_service.Program;
+using word_count;
+
+namespace finalHW_service
+{
+    // делаем невозможным осуществление одновременных действий по нескольким клиентам
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class CounterService : ICounterService
+    {
+        public Dictionary<string, int> wordCountFun(string[] data)
+        {
+            //с использованием нескольких потоков
+            var obj = typeof(word_count.wordCount).GetMethod("process_count_par", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+            //передаем массив строк
+            var result = obj?.Invoke(new word_count.wordCount(), new object[] { data });
+            Dictionary<string, int> res  = result as Dictionary<string, int>;
+            //проверка на результат
+            if (result != null && result.GetType() == typeof(Dictionary<string, int>))
+            {
+                return res;
+            }
+            else
+            {
+                return null;
+            }
+        }
+      
+
+    }
+       
+    
+}
+
---
+++ finalHW_service/finalHW_service/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+//using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalHW_service
+{
+    class Program
+    {
+        private static int BytsSize = 256000000;
+        
+
+        static void Main(string[] args)
+        {
+            
+            var uris = new Uri[1];
+            string address = "net.tcp://localhost:7061/CounterService";
+            uris[0] = new Uri(address);
+            ICounterService icounterservice = new CounterService();
+            ServiceHost servicehost = new ServiceHost(icounterservice, uris);
+            var binding = new NetTcpBinding(SecurityMode.None);
+            //стандартное время устраивает для 256 Мб
+            binding.MaxReceivedMessageSize = BytsSize;
+            binding.MaxBufferSize = BytsSize;
+            servicehost.AddServiceEndpoint(typeof(ICounterService), binding, "");
+            servicehost.Opened += Host_Opened;
+            servicehost.Open();
+            Console.ReadLine();
+        }
+
+        private static void Host_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("Service start");
+        }
+    }
+}
+
+
